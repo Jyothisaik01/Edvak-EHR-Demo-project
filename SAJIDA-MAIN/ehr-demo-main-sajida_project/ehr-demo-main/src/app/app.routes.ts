a9f1d6351a8a6b8f9de9d5848cde30e1db6073a4
Jyothisaik01 <karumajjijyothisai24042003@gmail.com>
2025-02-17 19:04:41 +0530

    Add files via upload

---
+++ SAJIDA-MAIN/ehr-demo-main-sajida_project/ehr-demo-main/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+
+export const routes: Routes = [
+    {
+        path:'login',
+        component:LoginComponent
+    },
+    
+    
+];
